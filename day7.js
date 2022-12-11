8a9cbe621f3ec7174714c312a3a5bc90d2bccc6e
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-12-11 11:00:35 +0000

    Day 7 test passing

--- day7.js
+++ day7.js
@@ -1,14 +1,32 @@
 function parse(input) {
   let lines = input.trimEnd().split("\n");
-  let files = {};
+  let root = {name: "/", files: {}, subDirectories: {}, parent: null};
+  let current = undefined;
   for(const line of lines) {
-    if (!line.startsWith('$')) {
+    console.log(line);
+    console.log(root);
+    if (line.startsWith('$')) {
+      console.log("Command " + line);
       let parts = line.split(" ");
-      console.log(parts);
-      files[parts[1]] = parseInt(parts[0]);
+      if (parts[1] === "cd") {
+        if (parts[2] === "..") {
+          current = current.parent;
+        } else {
+          current = (parts[2] == "/") ? root : current.subDirectories[parts[2]];
+        }
+      }
+    } else {
+      console.log("Not a command " + line);
+      let parts = line.split(" ");
+      if (parts[0] === "dir") {
+        current.subDirectories[parts[1]] = {name: parts[1], subDirectories: {}, files: {}, parent: current};
+      } else {
+        console.log(parts);
+        current.files[parts[1]] = parseInt(parts[0]);
+      }
     }
   }
-  return {name: "/", files: files, subDirectories: []};
+  return root;
 }
 
 module.exports = {parse}
--- test/day7.test.js
+++ test/day7.test.js
@@ -6,12 +6,13 @@ describe('Day 7', () => {
     let expected = {
       name: "/",
       files: {},
-      subDirectories: []
+      subDirectories: {},
+      parent: null
     };
     expect(result).toEqual(expected);
   });
 
-  it('should pass directory with files', () => {
+  it('should parse directory with files', () => {
     let input = "$ cd /\n" +
       "$ ls\n" +
       "14848514 b.txt\n" +
@@ -19,8 +20,74 @@ describe('Day 7', () => {
     let expected = {
       name: "/",
       files: {"b.txt": 14848514, "c.dat": 8504156},
-      subDirectories: []
+      subDirectories: {},
+      parent: null
     };
     expect(parse(input)).toEqual(expected);
+  });
+
+  it('should parse nested directory', () => {
+    let input = "$ cd /\n" +
+      "$ ls\n" +
+      "dir a\n" +
+      "14848514 b.txt\n" +
+      "8504156 c.dat\n" +
+      "$ cd a\n" +
+      "$ ls\n" +
+      "29116 f\n" +
+      "2557 g\n" +
+      "62596 h.lst";
+    let a = {
+      name: "a",
+      files: {"f": 29116, "g": 2557, "h.lst": 62596},
+      subDirectories: {}
+    };
+    let expected = {
+      name: "/",
+      files: {"b.txt": 14848514, "c.dat": 8504156},
+      subDirectories: {"a": a},
+      parent: null
+    };
+    a.parent = expected;
+    expect(parse(input)).toEqual(expected);
+  });
+
+  let exampleInput = "$ cd /\n" +
+    "$ ls\n" +
+    "dir a\n" +
+    "14848514 b.txt\n" +
+    "8504156 c.dat\n" +
+    "dir d\n" +
+    "$ cd a\n" +
+    "$ ls\n" +
+    "dir e\n" +
+    "29116 f\n" +
+    "2557 g\n" +
+    "62596 h.lst\n" +
+    "$ cd e\n" +
+    "$ ls\n" +
+    "584 i\n" +
+    "$ cd ..\n" +
+    "$ cd ..\n" +
+    "$ cd d\n" +
+    "$ ls\n" +
+    "4060174 j\n" +
+    "8033020 d.log\n" +
+    "5626152 d.ext\n" +
+    "7214296 k";
+
+  function exampleDirectoryStructure() {
+    let e = {name: "e", files: {"i": 584}, subDirectories: {}};
+    let a = {name: "a", files: {"f": 29116, "g": 2557, "h.lst": 62596}, subDirectories: {"e": e}};
+    e.parent = a;
+    let d = {name: "d", files: {"j": 4060174, "d.log": 8033020, "d.ext": 5626152, "k": 7214296}, subDirectories: {}};
+    let root = {name: "/", files: {"b.txt": 14848514, "c.dat": 8504156}, subDirectories: {"a": a, "d": d}, parent: null};
+    a.parent = root;
+    d.parent = root;
+    return root;
+  }
+
+  it('should parse full example', () => {
+    expect(parse(exampleInput)).toEqual(exampleDirectoryStructure());
   });
 })
