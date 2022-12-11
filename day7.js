171a64408105fb3bdc1c58fd4d8319fa78be21e7
Andrew Whitehouse <andrew@whitehouse.org.uk>
2022-12-11 09:28:05 +0000

    Second test passing.

--- day7.js
+++ day7.js
@@ -1,5 +1,14 @@
 function parse(input) {
-  return {name: "/", files: [], subDirectories: []};
+  let lines = input.trimEnd().split("\n");
+  let files = {};
+  for(const line of lines) {
+    if (!line.startsWith('$')) {
+      let parts = line.split(" ");
+      console.log(parts);
+      files[parts[1]] = parseInt(parts[0]);
+    }
+  }
+  return {name: "/", files: files, subDirectories: []};
 }
 
 module.exports = {parse}
--- test/day7.test.js
+++ test/day7.test.js
@@ -5,9 +5,22 @@ describe('Day 7', () => {
     let result = parse("$ cd /\n");
     let expected = {
       name: "/",
-      files: [],
+      files: {},
       subDirectories: []
     };
     expect(result).toEqual(expected);
-  })
+  });
+
+  it('should pass directory with files', () => {
+    let input = "$ cd /\n" +
+      "$ ls\n" +
+      "14848514 b.txt\n" +
+      "8504156 c.dat\n";
+    let expected = {
+      name: "/",
+      files: {"b.txt": 14848514, "c.dat": 8504156},
+      subDirectories: []
+    };
+    expect(parse(input)).toEqual(expected);
+  });
 })
